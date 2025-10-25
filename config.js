bad3ee1962e6a44f69f10761aaacd0d980d56d1d
DominicDev <s0l0estudi4r@gmail.com>
2025-10-25 12:33:40 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'JUSTICE';
+export let nameSongs = [
+	"D.A.N.C.E.", /*1*/
+	"GENESIS", /*2*/
+	"CIVILIZATION", /*3*/
+	"ON'N'ON", /*4*/
+	"AUDIO, VIDEO, DISCO.", /*5*/
+	"DVNO", /*6*/
+	"SAFE AND SOUND", /*7*/
+	"PLEASURE", /*8*/
+	"ALAKAZAM!", /*9*/
+	"FIRE", /*10*/
+	"STOP", /*11*/
+	"CHORUS", /*12*/
+	"RANDY", /*13*/
+	"HEAVY METAL", /*14*/
+	"S.0.S.", /*15*/
+	"CLOSE CALL ", /*16*/
+	"NEW LANDS", /*17*/
+	"WE ARE YOUR FRIENDS", /*18*/
+	"NEVERENDER", /*19*/
+	"DEAR ALAN", /*20*/
+]
